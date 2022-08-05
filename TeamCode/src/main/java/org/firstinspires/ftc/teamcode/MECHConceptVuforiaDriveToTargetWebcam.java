8e86f4754d4c88cc566c54f21f4fd8f95d40f708
gemp22 <60630258+gemp22@users.noreply.github.com>
2022-08-04 19:37:59 -0600

    1.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MECHConceptVuforiaDriveToTargetWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MECHConceptVuforiaDriveToTargetWebcam.java
@@ -40,7 +40,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  */
 
 @TeleOp(name="Drive To Target", group = "Concept")
-@Disabled
+//@Disabled
 public class MECHConceptVuforiaDriveToTargetWebcam extends LinearOpMode
 {
     // Adjust these numbers to suit your robot.
